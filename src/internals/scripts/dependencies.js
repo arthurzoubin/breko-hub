71b832bf1a70b1380056659bedc0885d8632e5eb
arthur.zou <arthur.zou@imaginato.com>
2017-06-21 08:46:51 +0700

    DLL

---
+++ src/internals/scripts/dependencies.js
@@ -0,0 +1,51 @@
+// No need to build the DLL in production
+if (process.env.NODE_ENV === 'production') {
+    process.exit(0);
+}
+
+require('babel-polyfill')
+require('babel-register')
+
+require('shelljs/global');
+
+const path = require('path');
+const fs = require('fs');
+const exists = fs.existsSync;
+const writeFile = fs.writeFileSync;
+
+const defaults = require('lodash/defaultsDeep');
+const pkg = require(path.join(process.cwd(), 'package.json'));
+const config = require('../../config/webpack.dll.config');
+
+const dllConfig = defaults(pkg.dllPlugin, config.dllPlugin.defaults);
+const outputPath = path.join(process.cwd(), dllConfig.path);
+const dllManifestPath = path.join(outputPath, 'package.json');
+
+/**
+ * I use node_modules/breko-hub by default just because
+ * it isn't going to be version controlled and babel wont try to parse it.
+ */
+
+mkdir('-p', outputPath);
+
+echo('Building the Webpack DLL...');
+
+/**
+ * Create a manifest so npm install doesn't warn us
+ */
+if (!exists(dllManifestPath)) {
+    writeFile(
+        dllManifestPath,
+        JSON.stringify(defaults({
+            name: 'breko-hub-dlls',
+            private: true,
+            author: pkg.author,
+            repository: pkg.repository,
+            version: pkg.version,
+        }), null, 2),
+        'utf8'
+    );
+}
+
+// the BUILDING_DLL env var is set to avoid confusing the development environment
+exec('cross-env BUILDING_DLL=true webpack --display-chunks --color --config src/config/webpack.dll.babel.js');
